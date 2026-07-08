3372de61f0d10740c08e30ea512f402f797ca32b
jktujq <odmamontov@gmail.com>
2026-07-08 12:14:53 +0300

    Markview support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -13,44 +13,29 @@ local opts = {
 		"json",
 		"yaml",
 		"markdown",
+		"latex",
 	},
 	auto_install = true,
 	highlight = {
 		enable = true,
-		additional_vim_regex_highlighting = true,
+	},
+	indent = {
+		enable = true,
 	},
 	incremental_selection = {
 		enable = true,
 		keymaps = {
 			init_selection = "<C-space>",
 			node_incremental = "<C-space>",
-			scope_incremental = false,
 			node_decremental = "<bs>",
 		},
 	},
-	indent = {
-		enable = true,
-	},
-	playground = {
-		enable = true,
-	},
 }
-
-local config = function()
-	vim.api.nvim_create_autocmd("ColorScheme", {
-		callback = function()
-			vim.treesitter.start()
-		end,
-	})
-end
 
 return {
 	"nvim-treesitter/nvim-treesitter",
-
 	build = ":TSUpdate",
-	event = { "BufReadPost", "BufNewFile" },
-	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+	cmd = { "TSInstall", "TSUpdate" },
 
 	opts = opts,
-	config = config,
 }
